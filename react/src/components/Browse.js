f6441d4461bc53af088d73fe81a7d5235eab91b6
Jasiu Leja <jleja09@gmail.com>
2018-02-02 12:54:05 -0500

    adds dummy Load More button

--- react/src/components/Browse.js
+++ react/src/components/Browse.js
@@ -4,6 +4,7 @@ import {GridTile} from 'material-ui/GridList';
 import Checkbox from 'material-ui/Checkbox';
 import ActionFavorite from 'material-ui/svg-icons/action/favorite';
 import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
+import FlatButton from 'material-ui/FlatButton/FlatButton';
 
 /*///
  COMPONENT
@@ -23,13 +24,21 @@ const Browse = (props) => {
     actionFavorite: {
       fill: '#c2a661',
     },
+    loadMoreButton: {
+      color: '#c2a661',
+      border: '4px solid #c2a661',
+      borderRadius: 20,
+      height: 60,
+      width: 200,
+      margin: 20,
+    },
   };
 
   return (
     <div>
       <h1 className="">Browse Items Page</h1>
       <div className="items-grid" style={styles.root}>
-      <div>
+        <div>
         { items && items.length ?
           items.map(item => (
             <div className="tile-container" key={item.id}>
@@ -51,6 +60,11 @@ const Browse = (props) => {
           : null
         }
         </div>
+        <FlatButton
+          label="Load More"
+          rippleColor="#c2a661"
+          style={styles.loadMoreButton}
+        />
       </div>
     </div>
   );
